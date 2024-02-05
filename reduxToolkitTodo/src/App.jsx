f826c037be2117cea2e27eee055b32f112158d56
Aman Sinha <1998amansinha@gmail.com>
2024-02-05 22:11:44 +0545

    reduxToolkit

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodos from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodos />
+      <Todos />
+    </>
+  )
+}
+
+export default App
