bde83066e5e98281a09e97300f2a1a9935b0d45b
Aman Sinha <1998amansinha@gmail.com>
2024-02-06 23:57:36 +0545

    Redux and react

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,16 +1,40 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import  AuthService  from "./appwrite/auth";
+import { Header, Footer } from "./components/index";
+import { Outlet } from 'react-router-dom'
+import './app.css'
 
 function App() {
+  const dispatch = useDispatch(); // For Connecting react and redux .
 
-  // Importing Environment URL
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true); // For Conditional Rendering, If loading is true show Loading icon if loading is false show data .
 
-  return (
-    <>
-      <h1>A blog app with Appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    AuthService
+    .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+        TODO : <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
- 
+export default App;
