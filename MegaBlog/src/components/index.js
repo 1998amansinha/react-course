98230a32dcca53e3c74c6f65e41a4cb5fc1c2e28
Aman Sinha <1998amansinha@gmail.com>
2024-02-08 22:18:47 +0545

    update

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -5,6 +5,8 @@ import Logo from "./Logo"
 import logoutBtn from "./Header/logoutBtn";
 import Button from "./Button"
 import Input from "./Input";
+import RTE from './RTE'
+import Select from './Select'
 
 export {
     Header,
@@ -13,5 +15,6 @@ export {
     Logo,
     logoutBtn,
     Button,
-    Input
+    Input,
+    RTE
 }
